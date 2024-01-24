2dea00d3d0fb99986c17f1d8911ea74cde8c6cbf
Laksana11 <laxsu1198@gmail.com>
2024-01-24 18:10:25 +0530

    Initial Commin

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#0067FF",
+        yellowColor: "#FEB60D",
+        purpleColor: "#9771FF",
+        irisBlueColor: "#01B5C5",
+        headingColor: "#181A1E",
+        textColor: "#4E545F",
+        greenColor: "0fb9b1",
+        berryColor: "#B53471",
+        greenColor: "#009432",
+        lightGreen: "#26de81",
+        paleGreen: "#1abc9c"
+      },
+      boxShadow: {
+        panelShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;",
+      },
+    },
+  },
+  plugins: [],
+}
+
