e8b071c0ad0698c9ec26d36ca805e1670fe9a188
Laksana11 <laxsu1198@gmail.com>
2024-01-31 18:31:50 +0530

    Third Commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -31,11 +31,11 @@ const Home = () => {
             hospitals. They conduct various tests to diagnose the ailment afflicting a person.
             </p>
 
-            <button className="btn">Request an Appoinment</button>
+            <button className="btn"> Get Start </button>
           </div>
 
           {/* ======== hero counter ======= */}
-          <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]" >
+          {/* <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]" >
             <div>
               <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700]
               text-headingColor">30+
@@ -59,7 +59,7 @@ const Home = () => {
               <span className="w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px]"></span>
               <p className="text__para">Patient Satisfaction</p>
             </div>
-          </div>
+          </div> */}
         </div>
 
         {/* ======== hero content ======= */}
@@ -68,10 +68,7 @@ const Home = () => {
           <div>
             <img className="w-full" src={heroImg01} alt="" />
           </div>
-          <div className="mt-[30px]">
-            <img src={heroImg02} alt="" className="w-full mb-[30px]"/>
-            <img src={heroImg03} alt="" className="w-full"/>
-          </div>
+      
         </div>
       </div>
     </div>
@@ -80,46 +77,18 @@ const Home = () => {
 
   <section>
     <div className="container">
-      <div className="lg:w-[470px] mx-auto">
-        <h2 className="heading text-center">Providing the best medical services</h2>
-        <p className="text__para text-center">
-          World-class care for everyone. Our health system offers unmatched,
-          expert jealth care.
-        </p>
-      </div>
+    
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-3 lg:gap-[15px] mt-[15px] lg:mt-[25px]">
         
-        <div className="py-[30px] px-5">
-          <div className="flex items-center justify-center">
-            <img src={icon01} alt="" />
-          </div>
-
-          <div className="mt-[30px">
-            <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
-              Find a Doctor
-            </h2>
-            <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
-              World-class care for everyone. Our health system offers unmatched,
-              expert jealth care. From the tab to the clinic.
-            </p>
-            
-            <Link to='/interviews' className="w-[44px] h-[44px] rounded-full border border-solid 
-            border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group 
-            hover:bg-primaryColor hover:border-none">
-                <BsArrowRight className="group-hover:text-white w-6 h-5" />
-            </Link>
-          </div>
-        </div>
-
-        <div className="py-[30px] px-5">
+        <div className="py-[20px] px-5">
           <div className="flex items-center justify-center">
             <img src={icon02} alt="" />
           </div>
 
-          <div className="mt-[30px">
+          <div className="mt-[20px]">
             <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
-              Find a Location
+              Front-end
             </h2>
             <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
               World-class care for everyone. Our health system offers unmatched,
@@ -134,14 +103,14 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="py-[30px] px-5">
+        <div className="py-[20px] px-5">
           <div className="flex items-center justify-center">
             <img src={icon03} alt="" />
           </div>
 
-          <div className="mt-[30px">
+          <div className="mt-[20px]">
             <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
-              Book Appoinment
+              Back-end
             </h2>
             <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
               World-class care for everyone. Our health system offers unmatched,
@@ -154,6 +123,70 @@ const Home = () => {
                 <BsArrowRight className="group-hover:text-white w-6 h-5" />
             </Link>
           </div>
+        </div>
+        <div className="py-[20px] px-5">
+          <div className="flex items-center justify-center">
+            <img src={icon01} alt="" />
+          </div>
+
+          <div className="mt-[20px]">
+            <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+              Database
+            </h2>
+            <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+              World-class care for everyone. Our health system offers unmatched,
+              expert jealth care. From the tab to the clinic.
+            </p>
+            
+            <Link to='/interviews' className="w-[44px] h-[44px] rounded-full border border-solid 
+            border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group 
+            hover:bg-primaryColor hover:border-none">
+                <BsArrowRight className="group-hover:text-white w-6 h-5" />
+            </Link>
+          </div>
+        </div>
+        <div className="py-[20px] px-5">
+          <div className="flex items-center justify-center">
+            <img src={icon01} alt="" />
+          </div>
+
+          <div className="mt-[20px]">
+            <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+              Data Structures
+            </h2>
+            <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+              World-class care for everyone. Our health system offers unmatched,
+              expert jealth care. From the tab to the clinic.
+            </p>
+            
+            <Link to='/interviews' className="w-[44px] h-[44px] rounded-full border border-solid 
+            border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group 
+            hover:bg-primaryColor hover:border-none">
+                <BsArrowRight className="group-hover:text-white w-6 h-5" />
+            </Link>
+          </div>
+
+          <div className="py-[20px] px-5">
+          <div className="flex items-center justify-center">
+            <img src={icon01} alt="" />
+          </div>
+
+          <div className="mt-[20px]">
+            <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+              Sorting Algorithms
+            </h2>
+            <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+              World-class care for everyone. Our health system offers unmatched,
+              expert jealth care. From the tab to the clinic.
+            </p>
+            
+            <Link to='/interviews' className="w-[44px] h-[44px] rounded-full border border-solid 
+            border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group 
+            hover:bg-primaryColor hover:border-none">
+                <BsArrowRight className="group-hover:text-white w-6 h-5" />
+            </Link>
+          </div>
+        </div>
         </div>
 
       </div>
